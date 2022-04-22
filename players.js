e14f55842076abd1c0fb6356b0e9f175ccdd9616
Gallifrey <sird.mallika@gmail.com>
2022-04-22 16:34:37 +0530

    fixed colors

--- players.js
+++ players.js
@@ -1,13 +1,13 @@
 data = `{
           "colors" : [
             [255, 251, 43],
-            [255, 71, 166],
-            [61, 255, 43],
-            [214, 236, 255],
             [252, 62, 48],
             [217, 79, 255],
             [26, 138, 61],
-            [79, 249, 255]
+            [79, 249, 255],
+            [255, 71, 166],
+            [61, 255, 43],
+            [214, 236, 255]
           ],
           "round1": ["Smriti","Sai Sudhir","Samarth","Rimzim","Nikhil","Divya","Nigel","Dhruthi","Yashwanth","Kashish","Pranav","Abhishek","Aditi","Sampurn","Sanket","Arnav"]
           }`;
