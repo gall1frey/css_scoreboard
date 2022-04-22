64da8d679b012deece2de5b348664ea23f20fc69
Gallifrey <sird.mallika@gmail.com>
2022-04-22 16:21:03 +0530

    first commit

---
+++ players.js
@@ -0,0 +1,19 @@
+data = `{
+          "colors" : [
+            [255, 251, 43],
+            [255, 71, 166],
+            [61, 255, 43],
+            [214, 236, 255],
+            [252, 62, 48],
+            [217, 79, 255],
+            [26, 138, 61],
+            [79, 249, 255]
+          ],
+          "round1": ["Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita"],
+          "round2": []
+          }`;
+
+/*idk = '{
+  "colors": [[255, 251, 43],[255, 71, 166],[61, 255, 43],[214, 236, 255],[252, 62, 48],[217, 79, 255],[26, 138, 61],[79, 249, 255]],
+  "round1": ["Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita","Gallifrey","Lauritz","Anuraag","Dishita"]
+}';*/
